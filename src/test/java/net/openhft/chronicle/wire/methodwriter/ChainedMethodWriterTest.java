5351f341fbcf1e12a37a634827fdb4df3e191d0b
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2020-07-15 16:18:02 +0100

    Improve support for generated proxies for chained method calls, https://github.com/OpenHFT/Chronicle-Wire/issues/172

---
+++ src/test/java/net/openhft/chronicle/wire/methodwriter/ChainedMethodWriterTest.java
@@ -0,0 +1,40 @@
+package net.openhft.chronicle.wire.methodwriter;
+
+import net.openhft.chronicle.bytes.Bytes;
+import net.openhft.chronicle.bytes.MethodReader;
+import net.openhft.chronicle.core.Mocker;
+import net.openhft.chronicle.wire.TextWire;
+import net.openhft.chronicle.wire.Wire;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ChainedMethodWriterTest {
+    @Test
+    public void toPublisher() {
+        Wire wire = new TextWire(Bytes.allocateElasticOnHeap());
+        ToPublisher toPublisher = wire.methodWriter(ToPublisher.class);
+        toPublisher.to(1234).say("hello");
+        assertEquals("to: 1234\n" +
+                        "say: Hello",
+                wire.bytes().toString());
+        StringBuilder sb = new StringBuilder();
+        MethodReader reader = wire.methodReader(Mocker.intercepting(ToPublisher.class, "", sb::append));
+        assertTrue(reader.readOne());
+        assertFalse(reader.readOne());
+        assertEquals("to: 1234\n" +
+                "say: Hello", sb.toString());
+    }
+
+    interface To<T> {
+        T to(long dest);
+    }
+
+    interface Publisher {
+        void say(String text);
+    }
+
+    interface ToPublisher extends To<Publisher> {
+
+    }
+}
